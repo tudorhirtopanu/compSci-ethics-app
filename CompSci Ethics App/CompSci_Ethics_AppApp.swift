67064e5e081dffffb660dab15479d39e3865223f
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-20 20:24:29 +0000

    Initial Commit

---
+++ CompSci Ethics App/CompSci_Ethics_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompSci_Ethics_AppApp.swift
+//  CompSci Ethics App
+//
+//  Created by Tudor Hirtopanu on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CompSci_Ethics_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CompSci Ethics App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  CompSci Ethics App
+//
+//  Created by Tudor Hirtopanu on 20/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            Text("Comp sci app")
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
