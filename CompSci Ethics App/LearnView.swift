96fb427159a2d23a796968c82609bbf9b0ba85e2
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-20 21:39:49 +0000

    Add learn view and quiz view

---
+++ CompSci Ethics App/LearnView.swift
@@ -0,0 +1,18 @@
+//
+//  LearnView.swift
+//  CompSci Ethics App
+//
+//  Created by Tudor Hirtopanu on 20/11/2023.
+//
+
+import SwiftUI
+
+struct LearnView: View {
+    var body: some View {
+        Text("Learn View")
+    }
+}
+
+#Preview {
+    LearnView()
+}
