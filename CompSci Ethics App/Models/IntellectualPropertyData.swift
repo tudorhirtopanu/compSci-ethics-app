fed1b9977d5ce5713e2a628d36e1b9a391e41028
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-21 22:34:56 +0000

    add model for json data

--- CompSci Ethics App/Models/IntellectualPropertyData.swift
+++ CompSci Ethics App/Models/IntellectualPropertyData.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class IntellectualPropertyData: Identifiable, Decodable {
+    var id:Int
+    var title:String
+    var definition:String
+    var examples:[String]
+    var purpose:[String]
+    var protectionOverview:[ProtectionOverview]
+    var typesOfProtection:[TypesOfProtection]
+}
+
+class ProtectionOverview: Identifiable, Decodable {
+    var id:Int
+    var name:String
+    var protects:[String]
+    var examples:[String]?
+}
+
+class TypesOfProtection: Identifiable, Decodable {
+    var id:Int
+    var name:String
+}
