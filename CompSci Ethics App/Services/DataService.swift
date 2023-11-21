9f9bb5a435056999a129910d87e50712c9497196
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-21 22:34:42 +0000

    Add function for parsing json

--- CompSci Ethics App/Services/DataService.swift
+++ CompSci Ethics App/Services/DataService.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+class DataService {
+    
+    static func getLocalData() -> [IntellectualPropertyData] {
+        
+        // Parse local json file
+        
+        // Get a url path to the json file
+        let pathString = Bundle.main.path(forResource: "IntellectualProperty", ofType: "json")
+        
+        // Check if pathString is not nil, otherwise...
+        guard pathString != nil else {
+            return [IntellectualPropertyData]()
+        }
+        
+        // Create a url object
+        let url = URL(fileURLWithPath: pathString!)
+        
+        do {
+            // Create a data object
+            let data = try Data(contentsOf: url)
+            
+            // Decode the data with a JSON decoder
+            let decoder = JSONDecoder()
+            
+            do {
+                
+                let recipeData = try decoder.decode([IntellectualPropertyData].self, from: data)
+                
+                // Return the recipes
+                return recipeData
+                
+            }
+            catch {
+                // error with parsing json
+                print(error)
+            }
+
+        }
+        catch {
+            //error  with getting data
+            print(error)
+        }
+        
+        return [IntellectualPropertyData]()
+    }
+    
+}
