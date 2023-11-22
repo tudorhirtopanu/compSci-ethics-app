760d6e12d077dc95b409fd015d8bff243c10622b
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-22 20:51:31 +0000

    complete all intellectual property intro section data

--- CompSci Ethics App/Models/IntellectualPropertyData.swift
+++ CompSci Ethics App/Models/IntellectualPropertyData.swift
@@ -12,19 +12,27 @@ class IntellectualPropertyData: Identifiable, Decodable {
     var title:String
     var definition:String
     var examples:[String]
+    var purposeTitle:String
     var purpose:[String]
+    var protectionDescription:String
     var protectionOverview:[ProtectionOverview]
     var typesOfProtection:[TypesOfProtection]
+    var copyright:Copyright
 }
 
 class ProtectionOverview: Identifiable, Decodable {
     var id:Int
     var name:String
     var protects:[String]
-    var examples:[String]?
+    var examples:String?
 }
 
 class TypesOfProtection: Identifiable, Decodable {
     var id:Int
     var name:String
+}
+
+class Copyright: Identifiable, Decodable {
+    var id:Int
+    var title:String
 }
