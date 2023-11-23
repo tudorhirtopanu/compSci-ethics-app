a22ad4c0ae4ee9cb5600c59e496e45e6503eef17
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-23 09:48:24 +0000

    complete copyright and patents data

---
+++ CompSci Ethics App/Views/Learning/IntellectualProperty/PatentsView.swift
@@ -0,0 +1,18 @@
+//
+//  PatentsView.swift
+//  CompSci Ethics App
+//
+//  Created by Tudor Hirtopanu on 23/11/2023.
+//
+
+import SwiftUI
+
+struct PatentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PatentsView()
+}
