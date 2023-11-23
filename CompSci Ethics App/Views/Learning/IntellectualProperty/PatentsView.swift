a14ba425143c06293db6862c1801fd8ed9319be4
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-23 09:50:25 +0000

    complete patents view

--- CompSci Ethics App/Views/Learning/IntellectualProperty/PatentsView.swift
+++ CompSci Ethics App/Views/Learning/IntellectualProperty/PatentsView.swift
@@ -6,13 +6,82 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct PatentsView: View {
+    
+    @EnvironmentObject var ipm:IntellectualPropertyManager
+    
+    let videoRatio: CGFloat = 1080/1920
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ScrollView {
+                VStack(alignment: .leading){
+                    if let data = ipm.intellectualPropertyData.first?.patents {
+                        
+                        Text("Chapter Three")
+                            .font(.callout)
+                            .opacity(0.6)
+                        
+                        Text(data.title)
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding(.bottom,5)
+                        
+                        Text(data.definition)
+                            .padding(.bottom,5)
+                        
+                        VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "Patents", withExtension: "mp4")!))
+                            .frame(height: geo.size.width * videoRatio)
+                            .cornerRadius(5)
+                        
+                        Text("Why Use Patents?")
+                            .font(.headline)
+                            .padding([.top, .bottom], 5)
+                        
+                        ForEach(data.patentFacts, id: \.self){ c in
+                                Text(c)
+                                    .padding(.bottom,2)
+                        }
+                        
+                        Text("Boundaries")
+                            .font(.headline)
+                            .padding([.top, .bottom], 5)
+                        
+                        Text(data.boundaries)
+                            .padding(.bottom, 5)
+                        
+                        ForEach(data.boundaryExample, id: \.self){ c in
+                            HStack {
+                                Text("• ")
+                                Text(c)
+                                    .padding(.bottom,2)
+                            }
+                        }
+                        
+                        NavigationLink(value: IntellectualPropertyChapters.patents, label: {
+                            ZStack {
+                                RectangleCard(color: .blue)
+                                    .frame(height: 40)
+                                    .padding(.horizontal, 5)
+                                
+                                Text("Finish")
+                                    .foregroundStyle(Color.white)
+                            }
+                        })
+                        .padding(.top, 10)
+                        
+                    }
+                }
+            }
+            .scrollIndicators(.hidden)
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
     PatentsView()
+        .environmentObject(IntellectualPropertyManager())
 }
