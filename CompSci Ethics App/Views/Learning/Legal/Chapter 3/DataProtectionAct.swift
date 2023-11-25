fb5e80a8e6e1ebcce315745d4b807dd3e4d6db5a
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-25 12:45:23 +0000

    complete dpa view

--- CompSci Ethics App/Views/Learning/Legal/Chapter 3/DataProtectionAct.swift
+++ CompSci Ethics App/Views/Learning/Legal/Chapter 3/DataProtectionAct.swift
@@ -8,11 +8,90 @@
 import SwiftUI
 
 struct DataProtectionAct: View {
+    
+    @EnvironmentObject var legalManager:LegalManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment:.leading){
+                if let data = legalManager.legalData.first?.dpa1998{
+                    
+                    Text(data.chapter)
+                        .font(.callout)
+                        .opacity(0.6)
+                    
+                    Text(data.title)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.bottom,5)
+                    
+                    Text(data.description)
+                        .padding(.bottom,5)
+                    
+                    ForEach(data.keyPrinciples, id:\.self){d in
+                        
+                        HStack {
+                            Text("•")
+                            Text(d)
+                        }
+                        .padding(.bottom, 2)
+                        
+                    }
+                    
+                    Text("Who Does the DPA Apply to?")
+                        .font(.headline)
+                        .padding(.bottom, 2)
+                    
+                    Text("The DPA applies to data held on:")
+                        .padding(.bottom, 2)
+                    
+                    ForEach(data.dpaAppliesToDataHeldOn, id:\.self){d in
+                        
+                        HStack {
+                            Text("•")
+                            Text(d)
+                        }
+                        .padding(.bottom, 2)
+                        
+                    }
+                    
+                    ForEach(data.dataStoringExamples, id:\.self){d in
+                        
+                        HStack {
+                            Text("e.g.")
+                            Text(d)
+                        }
+                        .padding(.bottom, 2)
+                        
+                    }
+                    
+                    Text("Subject Rights Amendments")
+                        .font(.headline)
+                        .padding(.bottom, 2)
+                    
+                    Text(data.subjectRights1994)
+                        .padding(.bottom, 2)
+                    
+                    Text("This changed in the 1998 amendment where the act: ")
+                        .padding(.bottom, 2)
+                    
+                    ForEach(data.subjectRights1998, id:\.self){d in
+                        
+                        HStack {
+                            Text("•")
+                            Text(d)
+                        }
+                        .padding(.bottom, 2)
+                        
+                    }
+                    
+                }
+            }.padding(.horizontal)
+        }.scrollIndicators(.hidden)
     }
 }
 
 #Preview {
     DataProtectionAct()
+        .environmentObject(LegalManager())
 }
