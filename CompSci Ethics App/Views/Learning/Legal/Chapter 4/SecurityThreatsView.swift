8438100bf9f0341b494a6650119504a5a289ec05
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-25 20:44:33 +0000

    complete security threats view

--- CompSci Ethics App/Views/Learning/Legal/Chapter 4/SecurityThreatsView.swift
+++ CompSci Ethics App/Views/Learning/Legal/Chapter 4/SecurityThreatsView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct SecurityThreatsView: View {
+    
+    @EnvironmentObject var legalManager:LegalManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            VStack(alignment:.leading){
+                if let data = legalManager.legalData.first?.securityThreats{
+                    
+                    Text(data.chapter)
+                        .font(.callout)
+                        .opacity(0.6)
+                    
+                    Text(data.title)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.bottom,5)
+                    
+                    ForEach(data.threats){ t in
+                        
+                        HStack {
+                            Text(String(t.id + 1))
+                                .font(.headline)
+                                .padding(.bottom, 5)
+                            
+                            Text(t.name)
+                                .font(.headline)
+                                .padding(.bottom, 5)
+                        }
+                        
+                        ForEach(t.examples, id:\.self) {e in
+                            HStack {
+                                Text("•")
+                                Text(e)
+                            }
+                            .padding(.bottom, 2)
+                        }
+                        
+                    }
+                    
+                }
+            }.padding(.horizontal)
+        }.scrollIndicators(.hidden)
+        
     }
 }
 
 #Preview {
     SecurityThreatsView()
+        .environmentObject(LegalManager())
 }
