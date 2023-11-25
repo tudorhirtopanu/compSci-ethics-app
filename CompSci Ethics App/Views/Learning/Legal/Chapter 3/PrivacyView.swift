934b78d275ae947d27189f3509a9cda29f2899db
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-25 12:45:54 +0000

    complete privacy view

--- CompSci Ethics App/Views/Learning/Legal/Chapter 3/PrivacyView.swift
+++ CompSci Ethics App/Views/Learning/Legal/Chapter 3/PrivacyView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct PrivacyView: View {
+    
+    @EnvironmentObject var legalManager:LegalManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            VStack(alignment:.leading){
+                if let data = legalManager.legalData.first?.privacy{
+                    
+                    Text(data.chapter)
+                        .font(.callout)
+                        .opacity(0.6)
+                    
+                    Text(data.title)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.bottom,5)
+                    
+                    Text(data.definition)
+                        .padding(.bottom,5)
+                    
+                    Text(data.privacyTemp)
+                        .padding(.bottom,5)
+                    
+                    Text("Issues Caused By Data")
+                        .font(.headline)
+                        .padding(.bottom, 2)
+                    
+                    ForEach(data.dataIssues, id:\.self){d in
+                        HStack {
+                            Text("•")
+                            Text(d)
+                        }
+                        .padding(.bottom, 2)
+                    }
+                    
+                    Text(data.solution)
+                    
+                }
+            }.padding(.horizontal)
+        }.scrollIndicators(.hidden)
+        
     }
 }
 
 #Preview {
     PrivacyView()
+        .environmentObject(LegalManager())
 }
