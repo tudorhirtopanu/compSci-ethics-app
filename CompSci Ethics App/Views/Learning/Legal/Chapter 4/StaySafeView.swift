bf16b76fdd4732ae1a341abfa02bb32b77cabfc7
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-25 21:10:31 +0000

    Add navigation and environment objects

---
+++ CompSci Ethics App/Views/Learning/Legal/Chapter 4/StaySafeView.swift
@@ -0,0 +1,18 @@
+//
+//  StaySafeView.swift
+//  CompSci Ethics App
+//
+//  Created by Tudor Hirtopanu on 25/11/2023.
+//
+
+import SwiftUI
+
+struct StaySafeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StaySafeView()
+}
