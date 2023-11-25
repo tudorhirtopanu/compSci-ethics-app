e7c26838e8781999326c87823a994d1c5f684da6
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-25 12:45:12 +0000

    complete dpa legislation view

--- CompSci Ethics App/Views/Learning/Legal/Chapter 3/DPALegislation.swift
+++ CompSci Ethics App/Views/Learning/Legal/Chapter 3/DPALegislation.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct DPALegislation: View {
+    
+    @EnvironmentObject var legalManager:LegalManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment:.leading){
+                if let data = legalManager.legalData.first?.dataProtectionLegislation{
+                    
+                    Text(data.chapter)
+                        .font(.callout)
+                        .opacity(0.6)
+                    
+                    Text(data.title)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.bottom,5)
+                    
+                    ForEach(data.description, id:\.self){d in
+                        
+                        HStack {
+                            Text("•")
+                            Text(d)
+                        }
+                        .padding(.bottom, 2)
+                        
+                    }
+                    
+                }
+            }.padding(.horizontal)
+        }.scrollIndicators(.hidden)
     }
 }
 
 #Preview {
     DPALegislation()
+        .environmentObject(LegalManager())
 }
