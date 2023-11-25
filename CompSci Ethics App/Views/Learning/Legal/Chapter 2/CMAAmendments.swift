2a7e1a474a3fe63699e36c7d26a006eb9c4e538b
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-25 12:44:43 +0000

    complete cma amendments view

--- CompSci Ethics App/Views/Learning/Legal/Chapter 2/CMAAmendments.swift
+++ CompSci Ethics App/Views/Learning/Legal/Chapter 2/CMAAmendments.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct CMAAmendments: View {
+    
+    @EnvironmentObject var legalManager:LegalManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment:.leading){
+                if let data = legalManager.legalData.first?.compMisuseActAmendments{
+                    
+                    Text(data.chapter)
+                        .font(.callout)
+                        .opacity(0.6)
+                    
+                    Text(data.title)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.bottom,5)
+                    
+                    Text(data.description)
+                        .padding(.bottom,5)
+                    
+                    Text(data.amendmentTitle)
+                        .font(.headline)
+                        .padding(.bottom, 2)
+                    
+                    ForEach(data.amendmentDescription, id:\.self){d in
+                        Text(d)
+                            .padding(.bottom, 2)
+                    }
+                    
+                    NavigationLink(value: LegalChapters.Privacy, label: {
+                        ZStack {
+                            RectangleCard(color: .blue)
+                                .frame(height: 40)
+                                .padding(.horizontal, 5)
+                            
+                            Text("Next: Privacy (Chapter 3)")
+                                .foregroundStyle(Color.white)
+                        }
+                    })
+                    .padding(.top, 10)
+
+                }
+            }.padding(.horizontal)
+        }.scrollIndicators(.hidden)
     }
 }
 
 #Preview {
     CMAAmendments()
+        .environmentObject(LegalManager())
 }
