4b03a30f7e0cac3461f5424836ceb66f36e10ea3
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-25 20:44:19 +0000

    complete reducing risks view

--- CompSci Ethics App/Views/Learning/Legal/Chapter 4/ReducingRisksView.swift
+++ CompSci Ethics App/Views/Learning/Legal/Chapter 4/ReducingRisksView.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct ReducingRisksView: View {
+    
+    @EnvironmentObject var legalManager:LegalManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            VStack(alignment:.leading){
+                if let data = legalManager.legalData.first?.reducingRisks{
+                    
+                    Text(data.chapter)
+                        .font(.callout)
+                        .opacity(0.6)
+                    
+                    Text(data.title)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .padding(.bottom,5)
+                    
+                    ForEach(data.riskReduction){r in
+                        HStack {
+                            Text(String(r.id + 1)+".")
+                                .font(.headline)
+                                .padding(.bottom, 5)
+                            
+                            Text(r.name)
+                                .font(.headline)
+                                .padding(.bottom, 5)
+                        }
+                        
+                        ForEach(r.description, id:\.self){d in
+                            HStack {
+                                Text("•")
+                                Text(d)
+                            }
+                            .padding(.bottom, 2)
+                        }
+                        
+                        if let info = r.extraInfo {
+                            ForEach(info, id:\.self){i in
+                                Text(i)
+                                    .padding(.bottom, 2)
+                            }
+                        }
+                        
+                    }
+                    
+                }
+            }
+            .padding(.horizontal)
+        }.scrollIndicators(.hidden)
+        
     }
 }
 
 #Preview {
     ReducingRisksView()
+        .environmentObject(LegalManager())
 }
