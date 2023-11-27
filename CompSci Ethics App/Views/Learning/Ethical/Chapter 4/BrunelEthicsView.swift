b18a4ea33b3ead1d94b80874b048bcf0fb9ef02c
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-27 19:46:32 +0000

    complete brunel ethics view

--- CompSci Ethics App/Views/Learning/Ethical/Chapter 4/BrunelEthicsView.swift
+++ CompSci Ethics App/Views/Learning/Ethical/Chapter 4/BrunelEthicsView.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct BrunelEthicsView: View {
+    
+    @EnvironmentObject var ethicsManager:EthicsManager
+    @EnvironmentObject var navigationManager:NavigationManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment:.leading){
+                
+                if let data = ethicsManager.ethicsData.first?.brunelEthics {
+                    
+                    Text(data.chapter)
+                        .font(.callout)
+                        .opacity(0.6)
+                    
+                    Text(data.title)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.bottom,5)
+                    
+                    Text(data.description)
+                        .padding(.bottom,5)
+                    
+                    ForEach(data.guidelinesToFollow){g in
+                        
+                        Text(g.title)
+                            .font(.headline)
+                            .padding(.bottom,2)
+                        
+                        ForEach(g.description, id:\.self) { d in
+                            
+                                Text(d)
+                                .padding(.bottom, 5)
+                            
+                        }
+                        
+                    }
+                    
+                    Button(action: {
+                        navigationManager.popToRoot()
+                    }, label: {
+                        ZStack {
+                            RectangleCard(color: .blue)
+                                .frame(height: 40)
+                                .padding(.horizontal, 5)
+                            
+                            Text("Finish")
+                                .foregroundStyle(Color.white)
+                        }
+                    })
+                    .padding(.top, 10)
+                    
+                }
+            }.padding(.horizontal)
+        }.scrollIndicators(.hidden)
     }
 }
 
 #Preview {
     BrunelEthicsView()
+        .environmentObject(EthicsManager())
+        .environmentObject(NavigationManager())
 }
