89e45d6740f228f88133f587b7e1a62142bda575
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-27 19:04:35 +0000

    create class for managing ethics data

--- CompSci Ethics App/ViewModels/EthicsManager.swift
+++ CompSci Ethics App/ViewModels/EthicsManager.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class EthicsManager:ObservableObject {
+    
+    @Published var ethicsData = [EthicsData]()
+    
+    init(){
+        self.ethicsData = DataService.getLocalData(fileName: "Ethics", fileType: "json", objectType: EthicsData.self)
+    }
+    
+}
