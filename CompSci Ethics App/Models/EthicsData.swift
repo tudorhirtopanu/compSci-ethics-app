f8de1ecc2f8d33dfe4c8d5854b21907b24ced70a
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-27 19:04:17 +0000

    create data model for ethics data

---
+++ CompSci Ethics App/Models/EthicsData.swift
@@ -0,0 +1,70 @@
+//
+//  EthicsData.swift
+//  CompSci Ethics App
+//
+//  Created by Tudor Hirtopanu on 27/11/2023.
+//
+
+import Foundation
+
+class EthicsData: Identifiable, Decodable {
+    var id:Int
+    var title:String
+    var ethicalBehaviour:EthicalBehaviour
+    var ethicsVsLegal:EthicalVsLegal
+    var professionalEthics: ProfessionalEthics
+    var brunelEthics:BrunelEthics
+}
+
+class EthicalBehaviour:Identifiable, Decodable {
+    var id:Int
+    var chapter:String
+    var title:String
+    var definition:[String]
+    var conflicts:[String]
+    var shortDescription:String
+    var computerEthics:ComputerEthics
+}
+
+class ComputerEthics :Identifiable, Decodable {
+    var id:Int
+    var title:String
+    var description:[String]
+    var example:String
+}
+
+class EthicalVsLegal : Identifiable, Decodable {
+    var id:Int
+    var chapter:String
+    var title:String
+    var description:[String]
+}
+
+class ProfessionalEthics : Identifiable, Decodable {
+    var id:Int
+    var chapter:String
+    var title:String
+    var description:[String]
+    var codeOfConductSections:[CodeOfConductSections]
+}
+
+class CodeOfConductSections : Identifiable, Decodable {
+    var id:Int
+    var title:String
+    var subtitle:String
+    var description:String
+}
+
+class BrunelEthics : Identifiable, Decodable {
+    var id:Int
+    var chapter:String
+    var title:String
+    var description:String
+    var guidelinesToFollow:[GuidelinesToFollow]
+}
+
+class GuidelinesToFollow: Identifiable, Decodable {
+    var id = UUID()
+    var title:String
+    var description:[String]
+}
---
+++ CompSci Ethics App/ViewModels/EthicsManager.swift
@@ -0,0 +1,8 @@
+//
+//  EthicsManager.swift
+//  CompSci Ethics App
+//
+//  Created by Tudor Hirtopanu on 27/11/2023.
+//
+
+import Foundation
