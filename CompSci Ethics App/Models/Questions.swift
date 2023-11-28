3285b5cf5000b4cd054fd348b49d5d23264a0400
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-28 21:19:54 +0000

    update json data structure

---
+++ CompSci Ethics App/Models/Questions.swift
@@ -0,0 +1,16 @@
+//
+//  Questions.swift
+//  CompSci Ethics App
+//
+//  Created by Tudor Hirtopanu on 28/11/2023.
+//
+
+import Foundation
+
+class Quiz: Identifiable, Decodable {
+
+}
+
+class Questions: Identifiable, Decodable {
+    
+}
