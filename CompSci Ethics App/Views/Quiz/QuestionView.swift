be3819d4e4155e891826995b473527a49d6cf540
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-11-29 23:08:06 +0000

    update json structure for questions

---
+++ CompSci Ethics App/Views/Quiz/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  CompSci Ethics App
+//
+//  Created by Tudor Hirtopanu on 29/11/2023.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionView()
+}
