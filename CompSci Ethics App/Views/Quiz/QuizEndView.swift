f8b822fe4d669502596c426be59637f436b58333
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-12-03 09:54:26 +0000

    add context and query for using swift data

--- CompSci Ethics App/Views/Quiz/QuizEndView.swift
+++ CompSci Ethics App/Views/Quiz/QuizEndView.swift
@@ -5,12 +5,17 @@
 //  Created by Tudor Hirtopanu on 01/12/2023.
 //
 
+import SwiftData
 import SwiftUI
 
 struct QuizEndView: View {
     
     @EnvironmentObject var nm:NavigationManager
+    @EnvironmentObject var qm:QuestionsManager
+    @Environment(\.modelContext) var context
     
+    
+            
     var body: some View {
         VStack {
             Text("End of Quiz")
@@ -21,10 +26,28 @@ struct QuizEndView: View {
                 Text("Back")
             })
         }
+        .onAppear{
+            addItem()
+        }
+        
+        
+        
     }
+    
+    private func addItem() {
+        
+        //let object = items.first(where: items.)
+        
+        //let newQuestions = questionNum + qm.quiz[qm.section.rawValue].questions.count
+        let item = ModuleData(name: qm.returnSectionDetails(sectionId: qm.section.rawValue), totalQuestions: qm.quiz[qm.section.rawValue].questions.count)
+        
+        context.insert(item)
+        
+    }
+    
 }
 
-#Preview {
-    QuizEndView()
-        .environmentObject(NavigationManager())
-}
+//#Preview {
+//    QuizEndView()
+//        .environmentObject(NavigationManager())
+//}
